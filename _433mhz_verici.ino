ed8396399f0e97010ac76641490a9d854debede1
robosimay <akinakcaoglu@gmail.com>
2016-11-13 22:46:59 +0200

    Add files via upload

---
+++ _433mhz_verici.ino
@@ -0,0 +1,22 @@
+#include <VirtualWire.h> 
+char*mesaj; 
+void setup()
+{
+   vw_set_ptt_inverted(true);
+   vw_set_tx_pin(12); 
+   vw_setup(4000);
+}
+void loop()
+{
+for(byte r = 0; r < 255; r++){
+    for(byte g = 255; g > -1; g--){
+        for(byte b = 0; b < 255; b++){
+            if (analogRead(0) > 250){ goto cik;}
+        }
+    }
+}
+cik:
+
+
+}
+
